e96e28d7c779f900ff48e8d4df4c6632ac88bf60
Ant Kenworthy <git@manchestermonkey.co.uk>
2016-06-01 16:52:42 +0100

    Initial add
    + check-updates.sh
    + readme.md

---
+++ check-updates.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname -s)
+
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+echo "<updates> <host>";
+echo "<name>$HOSTNAME</name>"
+echo -ne "<engine>"
+
+if [ -x /usr/bin/yum ]; then
+
+echo -ne "yum</engine>\n<packages>"
+
+
+yum -q list updates |tac |head --lines=-1 | while read i
+do
+ i=$(echo $i) #this strips off yum's irritating use of whitespace
+ if [ "${i}x" != "x" ]
+ then
+  UVERSION=${i#*\ }
+  UVERSION=${UVERSION%\ *}
+  PNAME=${i%%\ *}
+
+  echo -ne "<package>\n<name>$PNAME</name>\n"
+  echo -ne "<cur>$(yum -q list installed $PNAME | tail -n 1|awk '{print $2}')</cur>\n"
+  echo -ne "<next>$UVERSION</next>\n</package>\n"
+
+ fi
+done
+
+
+echo -ne "\n</packages>"
+
+elif [ -x /usr/bin/apt-get ]; then
+apt-get -qq update 
+echo -ne "apt</engine>\n<packages>\n"
+apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1:::$2:::$3\n"}'|awk -F':::' '{print "<package><name>"$1"</name><cur>"$2"</cur><next>"$3"</next></package>" }'
+
+
+echo -ne "</packages>\n"
+
+fi
+
+
+echo -ne "\n</host>\n</updates>"
---
+++ readme.md
@@ -0,0 +1,45 @@
+Check-Updates
+=============
+
+This script is intended to be run as part of an ansible playbook that gathers information on what software packages have outstanding upgrades.
+
+The script should run on any Redhat ( yum ) or Debian ( apt ) based system.
+
+
+Execution
+---------
+
+Obviously this can be run from the command line directly 
+
+or can be executed with wget to post the XML results to a processing system:
+
+```bash
+/usr/bin/wget -O /dev/null http://processing.host/path --post-data "$(check-updates.sh)"  --header="Content-Type:application/xml"
+```
+
+
+
+Output
+------
+
+The script will output the following data:
+
+```XML
+<?xml version="1.0" encoding="utf-8"?>
+<updates> <host>
+<name>a</name>
+<engine>apt</engine>
+<packages>
+<package><name>PackageName</name><cur>1.0</cur><next>2.0</next></package>
+...
+</package>
+</host>
+</updates>
+
+```
+
+* `engine` - This field may return `apt` or `yum` depending on the system its run on.
+
+* `cur` and `next` - These fields will show the current and upgraded package versions respectivly.
+
+
